fe255bb305959c380c66a5bee4278266f04ab48d
Isabelly <125043792+isabellybarbosac@users.noreply.github.com>
2024-07-28 18:10:42 -0300

    Add files via upload

---
+++ avaliacoes/avaliacao-05/avPmd05/aluno_db/bin/aluno_db.dart
@@ -0,0 +1,5 @@
+import 'package:aluno_db/aluno_db.dart' as aluno_db;
+
+void main(List<String> arguments) {
+  print('Hello world: ${aluno_db.calculate()}!');
+}
---
+++ avaliacoes/avaliacao-05/avPmd05/aluno_db/test/aluno_db_test.dart
@@ -0,0 +1,8 @@
+import 'package:aluno_db/aluno_db.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
